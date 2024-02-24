7133f8ba80db95b54c8eaf4e291ad41dedcfec6d
Pawan Kumar <pk89844@gmail.com>
2024-02-24 12:00:04 +0530

    Print List

--- bin/dsa.dart
+++ bin/dsa.dart
@@ -1,5 +1,12 @@
 import 'package:dsa/dsa.dart' as dsa;
 
+import 'linkedlist/linkedlist.dart';
+
 void main(List<String> arguments) {
-  print('Hello world: ${dsa.calculate()}!');
+  LinkedList myLinkedList = LinkedList(4);
+
+  myLinkedList.printList();
+  myLinkedList.getHead();
+  myLinkedList.getTail();
+  myLinkedList.getLength();
 }
--- bin/linkedlist/linkedlist.dart
+++ bin/linkedlist/linkedlist.dart
@@ -1,1 +1,34 @@
-class LinkedList {}
+import 'node.dart';
+
+class LinkedList {
+  Node? _head;
+  late Node _tail;
+  late int _length;
+
+  LinkedList(int value) {
+    Node newNode = Node(value);
+    _head = newNode;
+    _tail = newNode;
+    _length = 1;
+  }
+
+  void printList() {
+    Node? temp = _head;
+    while (temp != null) {
+      print(temp.value);
+      temp = temp.next as Node?;
+    }
+  }
+
+  void getHead() {
+    print('Head: ${_head?.value}');
+  }
+
+  void getTail() {
+    print('Tail: ${_tail.value}');
+  }
+
+  void getLength() {
+    print('Length: $_length');
+  }
+}
