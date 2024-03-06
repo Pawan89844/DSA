7a4918754d9411a72e37bd77737c20e0c803fcb7
Pawan Kumar <pk89844@gmail.com>
2024-03-06 21:33:25 +0530

    Set Method

--- bin/dsa.dart
+++ bin/dsa.dart
@@ -4,5 +4,10 @@ import 'hash/hash_table.dart';
 
 void main(List<String> arguments) {
   HashTable myHashTable = HashTable();
+  myHashTable.set('nails', 100);
+  myHashTable.set('tile', 50);
+  myHashTable.set('lumber', 80);
+  myHashTable.set('bolts', 200);
+  myHashTable.set('screws', 140);
   myHashTable.printTable();
 }
--- bin/hash/hash_table.dart
+++ bin/hash/hash_table.dart
@@ -13,19 +13,39 @@ class HashTable {
       print('$i:');
       HashNode? temp = _dataMap[i];
       while (temp != null) {
-        print('  {  ${temp.key} + = + ${temp.value} }');
+        print('  {${temp.key}= ${temp.value}}');
         temp = temp.next;
       }
     }
   }
 
   int _hash(String key) {
+    //1. Declare hash with 0
+    //2. Loop input keys characters
+    //3. Assign each char into asci value
+    //4. Multiply ASCII Value to any prime number and then
+    //   divide the length using modulus operator
+    //   so that it should return the value within the range of data length (0-7)
+    //5. return hash
     int hash = 0;
-    List<String> keyChar = key.trim().split('');
-    for (int i = 0; i < keyChar.length; i++) {
-      int asciValue = keyChar[i].codeUnitAt(i);
+    for (int keyChar in key.runes) {
+      int asciValue = keyChar;
       hash = (hash + asciValue * 23) % _dataMap.length;
     }
     return hash;
+  }
+
+  void set(String key, int value) {
+    int index = _hash(key);
+    HashNode newNode = HashNode(key, value);
+    if (_dataMap[index] == null) {
+      _dataMap[index] = newNode;
+    } else {
+      HashNode? temp = _dataMap[index];
+      while (temp != null) {
+        temp = temp.next;
+      }
+      temp?.next = newNode;
+    }
   }
 }
