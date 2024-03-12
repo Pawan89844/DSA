655f0e3127e2e1d4529b61be4ba29c2478865826
Pawan Kumar <pk89844@gmail.com>
2024-03-12 15:03:12 +0530

    Merge Sort

--- bin/dsa.dart
+++ bin/dsa.dart
@@ -1,12 +1,12 @@
 import 'package:dsa/dsa.dart' as dsa;
 import 'algorithms/bubble sort/buble_sort.dart';
+import 'algorithms/merge sort/merge_sort.dart';
 import 'binarysearchtree/binary_search_tree.dart';
 import 'hash/hash_table.dart';
 import 'heap/heap.dart';
 
 void main(List<String> arguments) {
-  List<int> array = [9, 1, 6, 7, 3, 2, 5, 8];
-
-  BubbleSort.insertionSort(array);
-  print(array);
+  List<int> array1 = [1, 9, 8];
+  List<int> array2 = [2, 6, 5];
+  print(MergeSort.merge(array1, array2));
 }
