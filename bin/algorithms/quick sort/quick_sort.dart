a32ecc9ce098a855bb530770fd47058399982488
Pawan Kumar <pk89844@gmail.com>
2024-03-14 11:29:26 +0530

    Pivot method

---
+++ bin/algorithms/quick sort/quick_sort.dart
@@ -0,0 +1,19 @@
+class QuickSort {
+  static void swap(List<int> array, int firstIndex, int secondIndex) {
+    int temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+  }
+
+  static int pivot(List<int> array, int pivotIndex, int endIndex) {
+    int swapIndex = pivotIndex;
+    for (int i = pivotIndex + 1; i <= endIndex; i++) {
+      if (array[i] < array[pivotIndex]) {
+        swapIndex++;
+        swap(array, swapIndex, i);
+      }
+    }
+    swap(array, pivotIndex, swapIndex);
+    return swapIndex;
+  }
+}
--- bin/dsa.dart
+++ bin/dsa.dart
@@ -1,12 +1,14 @@
 import 'package:dsa/dsa.dart' as dsa;
 import 'algorithms/bubble sort/buble_sort.dart';
 import 'algorithms/merge sort/merge_sort.dart';
+import 'algorithms/quick sort/quick_sort.dart';
 import 'binarysearchtree/binary_search_tree.dart';
 import 'hash/hash_table.dart';
 import 'heap/heap.dart';
 
 void main(List<String> arguments) {
-  List<int> array1 = [1, 9, 8];
-  List<int> array2 = [2, 6, 5];
-  print(MergeSort.merge(array1, array2));
+  List<int> array = [4, 6, 1, 7, 3, 2, 5];
+  int returnedIndex = QuickSort.pivot(array, 0, array.length - 1);
+  print('Returned Index $returnedIndex');
+  print('Array : $array');
 }
