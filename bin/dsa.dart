a0cda6dffc4fbe3950d93029c767f955ae00b45b
Pawan Kumar <pk89844@gmail.com>
2024-07-03 11:11:20 +0530

    Leetcode First occurence fix

--- bin/dsa.dart
+++ bin/dsa.dart
@@ -3,16 +3,16 @@ import 'hash/my_maps.dart';
 import 'strings/my_ds.dart';
 
 void main(List<String> arguments) {
-  MyMaps maps = MyMaps();
+  // MyMaps maps = MyMaps();
 
-  maps.intersection([1, 2, 2, 1], [2, 2]);
+  // maps.intersection([1, 2, 2, 1], [2, 2]);
   // Technique tq = Technique();
   // List<int> arr = [2, 1, 5, 1, 3, 2];
   // int k = 3;
   // tq.maxSum(arr, k);
-  // MyDS myString = MyDS();
+  MyDS myString = MyDS();
   // String name = 'Pawan Kumar';
-  // myString.patternMatching(name);
+  print(myString.strstr("sadbutsad", "sad"));
   // Graph graph = Graph();
   // graph.addVertex('A');
   // graph.addVertex('B');
--- bin/strings/my_ds.dart
+++ bin/strings/my_ds.dart
@@ -10,12 +10,32 @@ class MyDS {
     print('STRL $str');
   }
 
-  void patternMatching(String name) {
+  int strstr(String haystack, String needle) {
+    // int m = haystack.length - 1;
+    // int n = needle.length - 1;
+    for (int i = 0; i < haystack.length - needle.length + 1; i++) {
+      // print(haystack.substring(i, needle.length) == needle);
+      if (haystack.substring(i, needle.length) == needle) {
+        return i;
+      }
+    }
+    return -1;
+    // for (int i = 0; i < m; i++) {
+    //   if (haystack.substring(i, n) == needle) {
+    //     return i;
+    //   }
+    // }
+    // return -1;
+  }
+
+  void patternMatching(String haystack, String needle) {
+    // int n = haystack.compareTo(needle);
+
     // print('Name: ${}');
     // bool isPan = name.endsWith('Pawan');
     // int c = name.compareTo('Pawan Kumar');
 
-    print(name);
+    // print(name);
 
     // print('N: $n');
   }
