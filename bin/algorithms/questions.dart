004070dba2c195f0f3cd2a5532c9be7792d3c24d
Pawan Kumar <pk89844@gmail.com>
2024-07-09 23:10:42 +0530

    leetcode + 1 question

--- bin/algorithms/questions.dart
+++ bin/algorithms/questions.dart
@@ -262,4 +262,16 @@ class Questions {
     }
     return true;
   }
+
+  List<int> plusOne(List<int> digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+    digits.insert(0, 1);
+    return digits;
+  }
 }
--- bin/dsa.dart
+++ bin/dsa.dart
@@ -14,7 +14,7 @@ void main(List<String> arguments) {
   //     1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3, TreeNode(6)));
   // node.traverse(node);
   Questions q = Questions();
-  print(q.wordPattern("abba", "dog cat cat dog"));
+  print(q.plusOne([9]));
   // q.containsNearbyDuplicate([1, 2, 3, 1], 3);
   // print(q.passThePillow(4, 5));
 
